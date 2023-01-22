b188a6d2415d1a8bc65b5db3951ac2798e6cb87d
Wellington Defassio <well.defassio@gmail.com>
2023-01-22 20:37:47 -0300

    Initial commit

---
+++ src/test/java/io/git/welldefassio/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.git.welldefassio.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
